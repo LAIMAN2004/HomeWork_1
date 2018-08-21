9248d63ef3091229d19429925af7788d29254a16
Laiman <Laiman2004@gmail.com>
2018-08-21 14:47:32 +0300

    Initial commit

---
+++ src/Work_1/TempoName.java
@@ -0,0 +1,10 @@
+package Work_1;
+
+public class TempoName {
+    public static void main(String[] args) {
+        int test1; // объявление переменной и ее тип данных (int)
+        test1 = 7; // присвоение значению числа 7
+        int test2; // объявление второго занчение
+        test2 = (2+test1)*3;
+        System.out.println(test2);
+    }}
